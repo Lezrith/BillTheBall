c71bf9938dedcc8798a5b12fc03e969377e54755
Lezrith <krzysztof.g.tomczak@student.put.poznan.pl>
2015-05-18 16:29:56 +0200

    Reverting gravity as part of gameplay

--- Assets/Scripts/RevertGravity.cs
+++ Assets/Scripts/RevertGravity.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class RevertGravity : MonoBehaviour
 {
-
+    float currentPobability;
+    int rand;
+    int time;
+    public int meanTime;
     // Use this for initialization
     void Start()
     {
-
+        time = 0;
     }
-
+    void Update()
+    {
+        time++;
+        rand = Random.Range(1, 100);
+        currentPobability = 1 - Mathf.Pow(2, -time / meanTime);
+        if(rand<currentPobability*100)
+        {
+            time = 0;
+            Revert();
+        }
+        //Debug.Log((currentPobability * 100).ToString());
+    }
     public void Revert()
     {
         Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
